a58655378a88950045d2f5ca193035d8e3edaa1b
mn9142001 <75080962+mn9142001@users.noreply.github.com>
2022-08-01 16:34:29 +0200

    Update settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -109,7 +109,14 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
 
 # Password validation
